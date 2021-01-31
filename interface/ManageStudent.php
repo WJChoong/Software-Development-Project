422c095593b51d20f4db5325cd95ba9d6612251b
WJChoong <61902435+WJChoong@users.noreply.github.com>
2021-01-31 14:02:01 +0800

    Uploaded file

---
+++ interface/ManageStudent.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html>
+	<head>
+	</head>
+	
+	<?php require "../design/staff-navbar.php"?>
+	<body style="background-color: rgba(0,0,0,0.1);">
+		<div class="row bg-light">
+			<?php require "../design/navtab-managestudent.php"?>
+			<div class="col-md-9">
+				<div class="row">
+					<form action="" class="col-md-6 mr-auto">
+						<div class="input-group mb-3">
+							<div class="input-group-prepend">
+								<button class="btn btn-light dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">All</button>
+								<div class="dropdown-menu">
+								<?php
+									echo '<a class="dropdown-item disabled">Intake</a>';
+									echo '<div role="separator" class="dropdown-divider"></div>';
+									for ($i = 0; $i < 10; $i++){
+										echo '<a class="dropdown-item">'.$i.'</a>';
+									}
+								?>
+								  <div role="separator" class="dropdown-divider"></div>
+								  <a class="dropdown-item" href="#">Separated Category</a>
+								</div>
+							</div>
+							<input type="text" class="form-control" aria-label="Search input with dropdown button">
+							<div class="input-group-append">
+								<button class="btn btn-success" type="button">Search</button>
+							</div>
+						</div>
+					</form>
+					<a class="btn btn-lg btn-light ml-auto" href="#" style="border: solid 1px;">+</a>
+				</div>
+				<br><br>
+				<table class="table">
+					<thead>
+						<tr>
+							<th>No.</th>
+							<th>Student Name</th>
+							<th>Student ID</th>
+							<th>Intake</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php
+						 for ($i = 1;$i< 10;$i++){
+							echo'<tr>
+									<td>'.$i.'</td>
+									<td>Clark</td>
+									<td>Kent</td>
+									<td>UCDF1905ICT(SE)</td>
+									<td>
+										<a class="btn btn-sm btn-success" style="margin: 5px;" href="Student-View.php">View</a>
+										<a class="btn btn-sm btn-success" style="margin: 5px;" href="#">Edit</a>
+										<a class="btn btn-sm btn-success" style="margin: 5px;" href="#">Delete</a>
+									</td>
+								</tr>';
+						}
+						?>          
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ interface/Student-View.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Untitled Document</title>
+	</head>
+
+	<?php require "../design/staff-navbar.php"?>
+	<body style="background-color: rgba(0,0,0,0.1);">
+		<div class="row bg-light">
+			<?php require "../design/navtab-managestudent.php"?>
+			<div class="col-md-9">
+				<div class="container vh-100 bg-light" style="border-left: solid 1px; border-right: solid 1px;">
+					<picture style="margin: 10px;">
+						<img src="img/Logo-APU.png" class="img-fluid img-thumbnail rounded mx-auto d-block" alt="">
+					</picture>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
