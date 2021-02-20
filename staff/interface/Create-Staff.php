97cfc38226e94bdc286fd1da32566fe5ecc2b267
WJChoong <61902435+WJChoong@users.noreply.github.com>
2021-02-21 00:42:36 +0800

    add function-staff
    adding function to create staff profile

--- staff/interface/Create-Staff.php
+++ staff/interface/Create-Staff.php
@@ -3,8 +3,9 @@
 	<head>
 		<title>Untitled Document</title>
 	</head>
-
-		<?php require "../design/staff-navbar.php"?>
+	
+	<?php require "../action/conn.php"?>
+	<?php require "../design/staff-navbar.php"?>
 	<body style="background-color: rgba(0,0,0,0.1);">
 		<div class="row bg-light">
 			<?php require "../design/navtab-addstaff.php"?>
@@ -12,85 +13,93 @@
 				<div class="container bg-light card body">
 					<h3 class="row justify-content-center font-weight-bold">Create Information</h3>
 					<br>
-					<form action="" method="post">
+					<form action="../action/create-staff.php" method="post">
 						<div class="form-group row">
 							<label class="col-3 col-form-label">Name</label> 
 							<div class="col-8">
-							  <input placeholder="Name" class="form-control here" required="required" type="text">
+							  <input placeholder="Name" class="form-control here" name="name" type="text" required="required">
 							</div>
 						</div>
 						<div class="form-group row">
-							<label class="col-3 col-form-label">Student ID</label> 
-							<div class="col-8">
-						    	<input placeholder="Eg: TP012345" class="form-control here" type="text">
-							</div>
-					  	</div>
-						<div class="form-group row">
 							<label class="col-3 col-form-label">Date of Birth</label> 
 							<div class="col-8">
-								<input placeholder="dd/mm/yyyy" class="form-control here" type="text">
+								<input placeholder="dd/mm/yyyy" class="form-control here" name="dob" type="date">
 							</div>
 						</div>
 					    <div class="form-group row">
 					    <label class="col-3 col-form-label">Phone Number</label> 
 							<div class="col-8">
-								<input placeholder="Phone Number" class="form-control here" required="required" type="text">
+								<input placeholder="Phone Number" class="form-control here" name="phone" type="text" required="required">
 							</div>
 					    </div>
 						<div class="form-group row">
 							<label class="col-3 col-form-label">Gender</label> 
 							<div class="col-8">
-								<select class="custom-select">
+								<select class="custom-select" name="gender">
 									<option value="#">--Gender--</option>
-									<option value="male">Male</option>
-									<option value="female">Female</option>
+									<option value="M">Male</option>
+									<option value="F">Female</option>
 								</select>
 							</div>
 						</div>
 					    <div class="form-group row">
 							<label class="col-3 col-form-label">Email</label> 
 							<div class="col-8">
-								<input placeholder="Email" class="form-control here" required="required" type="text">
+								<input placeholder="Email" class="form-control here" name="email" type="text" required="required">
 							</div>
 					    </div>
 					    <div class="form-group row">
-							<label class="col-3 col-form-label">Address</label> 
+							<label class="col-3 col-form-label">Street</label> 
 							<div class="col-8">
-								<input placeholder="Address" class="form-control here" type="text">
+								<input placeholder="Street" class="form-control here" name="street" type="text">
 							</div>
 					    </div>
-					    <div class="form-group row">
-							<label class="col-3 col-form-label">Course</label> 
+						<div class="form-group row">
+							<label class="col-3 col-form-label">Postcode</label> 
 							<div class="col-8">
-								<select id="select" name="select" class="custom-select">
-									<option value="#">--Course Name--</option>
-									<option value="#">Male</option>
-									<option value="#">Female</option>
-								</select>
+								<input placeholder="PostCode" class="form-control here" name="postcode" type="text">
+							</div>
+					    </div>
+						<div class="form-group row">
+							<label class="col-3 col-form-label">City</label> 
+							<div class="col-8">
+								<input placeholder="City" class="form-control here" name="city" type="text">
+							</div>
+					    </div>
+						<div class="form-group row">
+							<label class="col-3 col-form-label">State</label> 
+							<div class="col-8">
+								<input placeholder="State" class="form-control here" name="state" type="text">
+							</div>
+					    </div>
+						<div class="form-group row">
+							<label class="col-3 col-form-label">Country</label> 
+							<div class="col-8">
+								<input placeholder="Country" class="form-control here" name="country" type="text">
 							</div>
 					    </div>
 					    <div class="form-group row">
 							<label class="col-3 col-form-label">Password</label> 
 							<div class="col-8">
-								<input placeholder="Password" class="form-control here" type="text">
+								<input placeholder="Password" class="form-control here" name="password" type="password">
 							</div>
 					    </div>
 						<div class="form-group row">
 							<label class="col-3 col-form-label">Confirm Password</label> 
 							<div class="col-8">
-								<input placeholder="Confirm Password" class="form-control here" type="text">
+								<input placeholder="Confirm Password" class="form-control here" name="password2" type="password">
 							</div>
 					    </div> 
 						<div class="form-group row">
 							<label class="col-3 col-form-label">Upload Your Photo</label> 
 							<div class="col-8">
-								<input type="file" class="custom-file-input" id="customFile">
+								<input class="custom-file-input" name="photo" id="customFile" type="file">
 								<label class="custom-file-label ml-3">Choose file</label>
 							</div>
 					    </div> 
 					    <div class="form-group row">
 							<div class="offset-4 col-8">
-								<button name="submit" type="submit" class="btn btn-primary">Create Profile</button>
+								<button class="btn btn-primary" name="submit" type="submit">Create Profile</button>
 							</div>
 					    </div>
 					</form>
